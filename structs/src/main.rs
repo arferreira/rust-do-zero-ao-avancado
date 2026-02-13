4234e61fb3dd371bf0d1162534b71ab2095afbe5
arferreira <arfs.antonio@gmail.com>
2026-02-13 13:45:38 -0500

    Adicionando aulas sobre structs
    Signed-off-by: arferreira <arfs.antonio@gmail.com>

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,25 +6,63 @@ struct User {
     active: bool,
 }
 
+impl User {
+    fn new(name: String, email: String) -> Self {
+        Self {
+            name,
+            email,
+            created_at: 42839,
+            active: false,
+        }
+    }
+
+    fn get_now() -> u32 {
+        123456
+    }
+
+    fn set_created_at(&mut self) -> bool {
+        self.created_at = User::get_now();
+        true
+    }
+
+    fn is_online(&self) -> bool {
+        self.active
+    }
+
+    fn set_email(&mut self, email: String) {
+        self.email = email
+    }
+
+    fn login(&mut self) {
+        self.active = true
+    }
+
+    fn logout(mut self) -> bool {
+        self.active = false;
+        self.active
+    }
+}
+
 fn main() {
-    let mut antonio = User {
-        name: String::from("Antonio"),
-        email: String::from("hello@antonio.land"),
-        created_at: 12345487,
-        active: true,
-    };
+    let mut antonio = User::new(String::from("Antonio"), String::from("hello@antonio.land"));
 
     antonio.name = String::from("Joao");
 
     println!("name is: {}", antonio.name);
 
-    if is_online(&antonio) {
+    antonio.set_email(String::from("email@joao.com"));
+
+    antonio.set_created_at();
+
+    println!("{:?}", antonio);
+
+    antonio.login();
+
+    if antonio.is_online() {
         println!("{} is online", antonio.name);
     } else {
         println!("{} is offline", antonio.name);
     }
-}
 
-fn is_online(user: &User) -> bool {
-    user.active
+    antonio.logout();
 }
